f1a530be346a7013a1f512d2716260107d9ab442
Vladimir Iglovikov <iglovikov@gmail.com>
2019-08-17 09:52:22 -0700

    Added py2cfg function

--- tests/test_config_parsers.py
+++ tests/test_config_parsers.py
@@ -1,4 +1,4 @@
-from helper_functions.config_parsing.from_py import py2dict
+from helper_functions.config_parsing.from_py import py2dict, py2cfg
 
 
 def test_py2dict():
@@ -9,3 +9,12 @@ def test_py2dict():
     shared_items = {k: target_dict[k] for k in target_dict if k in result_dict and target_dict[k] == result_dict[k]}
 
     assert len(shared_items) == len(result_dict)
+
+
+def test_py2cfg():
+    cfg = py2cfg('tests/data/temp_config.py')
+
+    assert cfg.a == 1
+    assert cfg.b == 3
+    assert cfg.c == range(10)
+

de00c5c03cc4e3f90727c8a995fcfc6dd887d18d
Vladimir Iglovikov <iglovikov@gmail.com>
2019-08-19 15:00:55 -0700

    bugfix

--- helper_functions/config_parsing/from_py.py
+++ helper_functions/config_parsing/from_py.py
@@ -15,11 +15,7 @@ class ConfigDict(Dict):
         try:
             value = super(ConfigDict, self).__getattr__(name)
         except KeyError:
-            ex = AttributeError(
-                "'{}' object has no attribute '{}'".format(
-                    self.__class__.__name__, name
-                )
-            )
+            ex = AttributeError("'{}' object has no attribute '{}'".format(self.__class__.__name__, name))
         except Exception as e:
             ex = e
         else:
@@ -52,9 +48,7 @@ def py2dict(file_path: (str, Path)) -> dict:
     """
     file_path = Path(file_path).absolute()
 
-    assert (
-        file_path.suffix == ".py"
-    ), f"Only Py file can be parsed, but got {file_path.name} instead."
+    assert file_path.suffix == ".py", f"Only Py file can be parsed, but got {file_path.name} instead."
 
     assert file_path.exists(), f"There is no file at the path {file_path}"
 
@@ -69,9 +63,7 @@ def py2dict(file_path: (str, Path)) -> dict:
 
     mod = import_module(module_name)
     sys.path.pop(0)
-    cfg_dict = {
-        name: value for name, value in mod.__dict__.items() if not name.startswith("__")
-    }
+    cfg_dict = {name: value for name, value in mod.__dict__.items() if not name.startswith("__")}
 
     return cfg_dict
 
--- tests/test_config_parsers.py
+++ tests/test_config_parsers.py
@@ -6,11 +6,7 @@ def test_py2dict():
 
     target_dict = dict(a=1, b=3, c=range(10))
 
-    shared_items = {
-        k: target_dict[k]
-        for k in target_dict
-        if k in result_dict and target_dict[k] == result_dict[k]
-    }
+    shared_items = {k: target_dict[k] for k in target_dict if k in result_dict and target_dict[k] == result_dict[k]}
 
     assert len(shared_items) == len(result_dict)
 

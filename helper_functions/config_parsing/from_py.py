aa3489c34b56c184fec22994ef0a7a8f8fb817f4
Vladimir Iglovikov <iglovikov@gmail.com>
2019-08-19 14:50:44 -0700

    Check for masks to exist

--- helper_functions/config_parsing/from_py.py
+++ helper_functions/config_parsing/from_py.py
@@ -8,7 +8,6 @@ from addict import Dict
 
 
 class ConfigDict(Dict):
-
     def __missing__(self, name):
         raise KeyError(name)
 
@@ -16,8 +15,11 @@ class ConfigDict(Dict):
         try:
             value = super(ConfigDict, self).__getattr__(name)
         except KeyError:
-            ex = AttributeError("'{}' object has no attribute '{}'".format(
-                self.__class__.__name__, name))
+            ex = AttributeError(
+                "'{}' object has no attribute '{}'".format(
+                    self.__class__.__name__, name
+                )
+            )
         except Exception as e:
             ex = e
         else:
@@ -50,14 +52,16 @@ def py2dict(file_path: (str, Path)) -> dict:
     """
     file_path = Path(file_path).absolute()
 
-    assert file_path.suffix == '.py', f"Only Py file can be parsed, but got {file_path.name} instead."
+    assert (
+        file_path.suffix == ".py"
+    ), f"Only Py file can be parsed, but got {file_path.name} instead."
 
     assert file_path.exists(), f"There is no file at the path {file_path}"
 
     module_name = file_path.stem
 
-    if '.' in module_name:
-        raise ValueError('Dots are not allowed in config file path.')
+    if "." in module_name:
+        raise ValueError("Dots are not allowed in config file path.")
 
     config_dir = str(file_path.parent)
 
@@ -66,9 +70,7 @@ def py2dict(file_path: (str, Path)) -> dict:
     mod = import_module(module_name)
     sys.path.pop(0)
     cfg_dict = {
-        name: value
-        for name, value in mod.__dict__.items()
-        if not name.startswith('__')
+        name: value for name, value in mod.__dict__.items() if not name.startswith("__")
     }
 
     return cfg_dict

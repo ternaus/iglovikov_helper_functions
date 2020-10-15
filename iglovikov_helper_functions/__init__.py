bbda4a38481dece9b56288b24cbece2bdc6904d0
Vladimir Iglovikov <iglovikov@gmail.com>
2020-10-15 16:57:00 -0700

    Added 2D rectangle intersection

--- iglovikov_helper_functions/__init__.py
+++ iglovikov_helper_functions/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
 
-__version__ = "0.0.46"
+__version__ = "0.0.47"
---
+++ iglovikov_helper_functions/utils/geometry_utils.py
@@ -0,0 +1,93 @@
+from typing import Union, List
+
+import numpy as np
+
+
+class Line:
+    # ax + by + c = 0
+    def __init__(self, v1: np.ndarray, v2: np.ndarray) -> None:
+        self.a = v2[1] - v1[1]
+        self.b = v1[0] - v2[0]
+        self.c = np.cross(v2, v1)
+
+    def __call__(self, point: np.ndarray) -> bool:
+        """
+        Any point p with line(p) <= 0 is on the "inside" (or on the boundary),
+        any point p with line(p) > 0 is on the "outside".
+
+
+        Args:
+            point:
+
+        Returns:
+
+        """
+        return self.a * point[0] + self.b * point[1] + self.c
+
+    def intersection(self, other: "Line") -> float:
+        # https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Using_homogeneous_coordinates
+
+        w = self.a * other.b - self.b * other.a
+
+        if w == 0:
+            return np.array([0, 0])
+
+        return np.cross([self.a, self.b, self.c], [other.a, other.b, other.c])[:2] / w
+
+    def __repr__(self):
+        return f"({self.a}) * x + ({self.b}) * y + ({self.c}) = 0"
+
+
+def intersection_rectangles(
+    rectangle_a: Union[np.ndarray, List[List[float]], List[np.ndarray]],
+    rectangle_b: Union[np.ndarray, List[List[float]], List[np.ndarray]],
+) -> float:
+    """Finds intersection of two rotated rectangles.
+
+    https://stackoverflow.com/a/45268241/2276600
+
+    Args:
+        rectangle_a: 4 (x, y) points in order
+        rectangle_a: 4 (x, y) points in order
+
+    Returns:
+
+    """
+
+    # Use the vertices of the first rectangle as
+    # starting vertices of the intersection polygon.
+    intersection = rectangle_a
+
+    # Loop over the edges of the second rectangle
+    for p, q in zip(rectangle_b, rectangle_b[1:] + rectangle_b[:1]):
+        if len(intersection) <= 2:
+            break  # No intersection
+
+        line = Line(p, q)
+
+        # Any point p with line(p) <= 0 is on the "inside" (or on the boundary),
+        # any point p with line(p) > 0 is on the "outside".
+
+        # Loop over the edges of the intersection polygon,
+        # and determine which part is inside and which is outside.
+        new_intersection = []
+        line_values = [line(t) for t in intersection]
+
+        for s, t, s_value, t_value in zip(
+            intersection, intersection[1:] + intersection[:1], line_values, line_values[1:] + line_values[:1]
+        ):
+            if s_value <= 0:
+                new_intersection.append(s)
+            if s_value * t_value < 0:
+                # Points are on opposite sides.
+                # Add the intersection of the lines to new_intersection.
+                intersection_point = line.intersection(Line(s, t))
+                new_intersection.append(intersection_point)
+
+        intersection = new_intersection
+
+    # Calculate area
+    if len(intersection) <= 2:
+        return 0
+
+    return 0.5 * sum(p[0] * q[1] - p[1] * q[0] for p, q in zip(intersection, intersection[1:] + intersection[:1]))
